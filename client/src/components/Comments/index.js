8fdb42b7b6fc7b865785c7416644433c250b47fd
mcramileux <kristineramilo21@gmail.com>
2023-08-31 20:24:26 +1000

    changed to Comments and updated it

---
+++ client/src/components/Comments/index.js
@@ -0,0 +1,33 @@
+// ADDING THIS AGAIN FOR THE CRITERIA
+import React from 'react';
+import CommentList from './CommentList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_COMMENTS } from '../utils/queries';
+
+const Comments= ({albumId}) => {
+  const { loading, data } = useQuery(QUERY_COMMENTS, {
+    variables: {albumId: albumId}
+  });
+  const comments = data?.comments || [];
+
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>LOADING REVIEWS...</div>
+          ) : (
+            <CommentList
+              comments={comments}
+              albumId={albumId}
+              title="Music Album Reviews"
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Comments;
